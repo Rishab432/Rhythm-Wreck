aed808dfb88ec78a6cf67ae1b90fd5857df6aff6
Sh0w3r15 <tylerwen006@gmail.com>
2024-01-22 01:43:48 -0500

    Added background video and miss sound effect

--- Assets/Tyler's Assets/TylerScripts/GameManager.cs
+++ Assets/Tyler's Assets/TylerScripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     private int _missesInARow;
     public float delay = 3f;
     [SerializeField] private AudioSource _beatmapAudio;
+    [SerializeField] private AudioSource _missAudio;
 
     void Start()
     {
@@ -21,13 +22,20 @@ public class GameManager : MonoBehaviour
             Debug.Log("dsdsadsdsds");
             SceneManager.LoadScene("Start");
         }
+        if (NoteActuator.Missed && NoteActuator.Combo >= 5)
+            _missAudio.Play();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        if (NoteActuator.Combo >= 5)
+        {
+            _missAudio.Play();
+            Debug.Log("misssound");
+
+        }
         Destroy(col.gameObject);
         NoteActuator.Combo = 0;
-        Debug.Log(NoteActuator.Combo);
         _missesInARow += 1;
     }
 
--- Assets/Tyler's Assets/TylerScripts/NoteActuator.cs
+++ Assets/Tyler's Assets/TylerScripts/NoteActuator.cs
@@ -10,6 +10,7 @@ public class NoteActuator : MonoBehaviour
     private int Score;
     static public int TotalScore;
     bool active = false;
+    static public bool Missed;
 
     GameObject note;
     Color old;
@@ -37,7 +38,12 @@ public class NoteActuator : MonoBehaviour
                 TotalScore = TotalScore + Score; 
             }
             else
+            {
+                Missed = true;
                 Combo = 0;
+            }
+
+                
         }
         if (Input.GetKeyUp(key))
             GetComponent<SpriteRenderer>().color = old;
