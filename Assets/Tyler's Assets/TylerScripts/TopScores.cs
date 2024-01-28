52fc07406339f4f40ae7fafc5f7bdbf09268224b
Sh0w3r15 <tylerwen006@gmail.com>
2024-01-28 13:51:14 -0500

    tops ore

---
+++ Assets/Tyler's Assets/TylerScripts/TopScores.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TopScores : MonoBehaviour
+{
+     [SerializeField] private Text _topScore1;
+     [SerializeField] private Text _topScore2;
+     [SerializeField] private Text _topScore3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
